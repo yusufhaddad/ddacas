81593049d91be2bbb88b63be84130bf5a18210b8
yusufhaddad <38319055+yusufhaddad@users.noreply.github.com>
2018-04-13 16:02:31 +0800

    Update signup.inc.php

--- Includes/signup.inc.php
+++ Includes/signup.inc.php
@@ -1,7 +1,8 @@
 <?php
 
 if (isset($_POST['submit'])) {
-	$dbServername = "yusufddac.mysql.database.azure.com";
+
+$dbServername = "yusufddac.mysql.database.azure.com";
 $dbUsername = "yusuf@yusufddac";
 $dbPassword = "Abc12345";
 $dbName = "ddac";
@@ -10,20 +11,13 @@ $dbName = "ddac";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-  //getting data from the form..
-  //mysqli_real_escape_string convert anything to string for security purpose...
-/*
-	$name = mysqli_real_escape_string($conn, $_POST['uname']);
-	$email = mysqli_real_escape_string($conn, $_POST['uemail']);
-	$password = mysqli_real_escape_string($conn, $_POST['upassword']);
-	$address = mysqli_real_escape_string($conn, $_POST['uaddress']);
-	$contact = mysqli_real_escape_string($conn, $_POST['ucontact']);
-*/
-	$user =  $_POST['user_name'];
-	$email = $_POST['uemail'];
-	$password = $_POST['pwd'];
-	$address = $_POST['uaddress'];
-	$contact = $_POST['ucontact'];
+
+
+	$name =  $_POST['username1'];
+	$email = $_POST['useremail1'];
+	$password = $_POST['passwd1'];
+	$address = $_POST['useraddress1'];
+	$contact = $_POST['usercontact1'];
 
 	//Error handling
 	//check for empty fields
@@ -36,7 +30,7 @@ $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 	} else{
     //check if input characters are valid..USING PREG_MATCH
     //checking name
-		if (!preg_match("/^[a-zA-Z]*$/" , $name)){
+		if (!preg_match("/^[a-zA-Z]*$/", $name)){
 
 		header("Location: ../signup.php?signup=invalid_Input");
 		exit();
@@ -46,7 +40,7 @@ $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 				header("Location: ../signup.php?signup=email_Format");
 				exit();
 			}else{
-				$sql = "SELECT * FROM users WHERE user_email='$email'";
+				$sql = "SELECT * FROM users1 WHERE user_email='$email'";
 				$result = mysqli_query($conn, $sql);
 				$resultCheck = mysqli_num_rows($result);
         //validate available users
@@ -54,12 +48,12 @@ $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 							header("Location: ../signup.php?signup=user_exist");
 							exit();
 				}else{
-					//Hashing password
-					$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+				
           //insert user data into Database
-					$sql1 = "INSERT INTO users (user_address,  user_contact, user_email,user_name, user_password) VALUES ('$name','$email', '$hashedPassword', '$address', '$contact');";
-					mysqli_query($conn, $sql1);
-					header("Location: ../signup.php?signup=success");
+					$sql11 = "INSERT INTO users1 (user_name, user_email, user_password, user_address, user_contact) VALUES ('$name','$email', 
+					$password, '$address', '$contact');";
+					mysqli_query($conn, $sql11);
+					header("Location: ../signup.php?signup=success1");
 					exit();
 				}
 			}
